b4118b5aec86939b1d4c41d0018488337f2556a8
Maslovars <maslovars@gmail.com>
2023-06-27 02:27:45 +0300

    fix: cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -3,8 +3,8 @@ import { fork } from 'child_process';
 const spawnChildProcess = async (args) => {
     const path = './src/cp/files/script.js';
     const child = fork(path, args, { silent: true });
-    process.stdout.pipe(child.stdin);
+    process.stdin.pipe(child.stdin);
     child.stdout.pipe(process.stdout);
 };
 
-spawnChildProcess();
+spawnChildProcess(['1', '2', '3', '4', '5',]);
