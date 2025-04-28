cd3236ddee53d18ef2222e32ea76b17c12699649
Maslovars <maslovars@gmail.com>
2025-04-29 02:25:51 +0300

    refactor: cp path

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,10 +1,11 @@
 import { fork } from 'child_process';
+import path from 'path';
 
 const spawnChildProcess = async (args) => {
-    const path = './src/cp/files/script.js';
-    const child = fork(path, args, { silent: true });
+    const filePath = path.join('src', 'cp', 'files', 'script.js');
+    const child = fork(filePath, args, { silent: true });
     process.stdin.pipe(child.stdin);
     child.stdout.pipe(process.stdout);
 };
 
-spawnChildProcess(['1', '2', '3', '4', '5',]);
+spawnChildProcess(['1', '2', '3', '4', '5']);
